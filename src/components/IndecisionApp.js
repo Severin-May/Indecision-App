a219fd0cfc17497f31865a7e26e776b8e35ef732
Aichurok <aichurokanatbekova@gmail.com>
2023-11-26 21:27:24 +0100

    changed subtitle

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -64,7 +64,7 @@ export default class IndecisionApp extends React.Component {
 
     render() {
         const title = "Indecision-App";
-        const subtitle = "Put your life in the hands of a computer!"
+        const subtitle = "Welcome! Put your life in the hands of a computer!"
         return (
             <div>
                 <Header title={title} subtitle={subtitle}/>
